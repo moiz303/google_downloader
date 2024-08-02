2667e898a3b8fa2079e544eeaa2cf2e95ad57d2a
moiz303 <moiz.izraylit@yandex.ru>
2024-08-02 12:40:29 +0300

    Unresolved files

---
+++ choose.py
@@ -0,0 +1,41 @@
+from PIL import Image
+
+
+def make_hash(im_name):
+        img = Image.open(im_name)
+
+        img2 = img.resize((10, 10))
+        pixels = img2.load()
+        img.close()
+
+        all_pixes = get_grey(pixels)
+        mid = sum(all_pixes) / 100
+
+        return get_bits(mid, all_pixes)
+
+
+def get_grey(pixels) -> list:
+    all_pixs = []
+    for i in range(10):
+        for j in range(10):
+            r, g, b = pixels[i, j]
+            bw = (r + g + b) // 3
+            pixels[i, j] = bw, bw, bw
+            all_pixs.append(bw)
+    return all_pixs
+
+
+def get_bits(mid, all_pixes) -> str:
+    bits = []
+    for i in range(100):
+        bits.append(str(int(all_pixes[i] > mid)))
+    return ''.join(bits)
+
+
+hash1 = make_hash("image_1.jpg")
+hash2 = make_hash("image_2.jpg")
+cou = 0
+for i in range(100):
+    if hash1[i] == hash2[i]:
+        cou += 1
+print(str(cou) + '%')
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import json
+
 import fake_useragent
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 
 
 def chromer(login: str, wordpass: str):
     """Авторизация для браузера Chrome"""
-    user = fake_useragent.UserAgent().random
+    user = fake_useragent.UserAgent().random  # Создаём фейкового юзера
 
-    options = Options()
+    options = Options()  # Создаём параметры для большей реалистичности
     options.add_argument(f"--user-agent={user}")
     options.add_argument("start-maximized")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("useAutomationExtension", False)
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)  # Запускаем бота
 
     driver.get(
         'https://accounts.google.com/v3/signin/identifier?elo=1&ifkv=AdF4I755v4RPrRuYlFe6D5bYHOhIc4OQDa0WI_Vf9lv-5jGGpP1V9O8V3EAYZjumAigcA9nVP7dP&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin&continue=https%3A%2F%2Faccounts.google.com%2FManageAccount%3Fnc%3D1')
@@ -23,19 +28,34 @@ def chromer(login: str, wordpass: str):
     email.send_keys(login)
 
     nextBtn = driver.find_element(value='identifierNext')
-    nextBtn.click()
+    nextBtn.click()  # Отправка логина
     time.sleep(5)
 
     password = driver.find_element(by='name', value='Passwd')
     password.send_keys(wordpass)
 
     nextBtn = driver.find_element(value='passwordNext')
-    nextBtn.click()
+    nextBtn.click()  # Отправка пароля
+    time.sleep(20)
+
+    driver.get_cookies()
+    driver.get("https://takeout.google.com/settings/takeout/custom/photos?utm_medium=organic-nav&utm_source=google-photos&hl=ru")
     time.sleep(5)
 
-    nextBtn = driver.find_element(by='href', value='https://photos.google.com/?authuser=0')
+    nextBtn = driver.find_element(by='xpath', value="//*[text() = 'Продолжить']")
     nextBtn.click()
-    time.sleep(10)
+    time.sleep(3)
+
+    select = Select(driver.find_element(By.CSS_SELECTOR, value="[aria-label='File size']"))
+    select.select_by_visible_text("50 ГБ")
+    time.sleep(3)
+
+    nextBtn = driver.find_element(by='xpath', value="//*[text() = 'Создать экспорт']")
+    nextBtn.click()
+    time.sleep(3)
+
+    driver.delete_all_cookies()
+    driver.quit()
 
 
 path_to_chrome_driver = 'apath/awebdriver.exe'
